3c7baceccf739bf051f6749b81643b37a9ea3c04
arkachat <arka13chatterjee@gmail.com>
2023-10-19 13:52:16 -0400

    added the chart and the .lock file function to not create multiple instances and wait untill the last process finishes

--- pod_cleaner.sh
+++ pod_cleaner.sh
@@ -1,33 +1,58 @@
 #!/bin/bash
 
-# Function to restart a pod
-restart_pod() {
-  namespace=$1
-  pod_name=$2
+# Define lock file location
+LOCKFILE="/tmp/pod_cleaner.lock"
 
-  # Deleting a pod to restart it, as the deployment/statefulset will create a new pod to replace it.
-  kubectl delete pod "$pod_name" -n "$namespace"
+# Check if lock file exists at script start
+if [ -f "$LOCKFILE" ]; then
+    echo "Lock file exists, previous run not finished. Exiting."
+    exit 1
+fi
+
+# Create lock file
+touch $LOCKFILE
+
+# Define cleanup procedure
+cleanup() {
+    echo "Cleaning up..."
+    rm -f $LOCKFILE
+    exit
 }
 
-# Main script
-while true; do
-  # Get all namespaces
-  namespaces=$(kubectl get namespaces -o jsonpath='{.items[*].metadata.name}')
+# Register the cleanup function for script exit, SIGHUP, SIGINT, SIGQUIT, SIGABRT, SIGTERM
+trap cleanup EXIT SIGHUP SIGINT SIGQUIT SIGABRT SIGTERM
 
-  for namespace in $namespaces; do
-    # Skip the kube-system namespace
-    if [ "$namespace" == "kube-system" ]; then
-      continue
+# Your existing script logic starts here
+
+# List of namespaces to ignore
+IGNORE_NAMESPACES=(kube-system)
+
+# Function to check if an item exists in an array
+in_array() {
+    local needle=$1
+    shift
+    local item
+    for item in "$@"; do
+        [[ $item == $needle ]] && return 0 # 0 is the success exit status in bash
+    done
+    return 1 # 1 is the general failure exit status in bash
+}
+
+# Get all namespaces
+NAMESPACES=$(kubectl get ns -o jsonpath='{.items[*].metadata.name}')
+
+for NAMESPACE in $NAMESPACES; do
+    # Skip the ignored namespace with IGNORED_NAMESPACES variable
+    if in_array "$NAMESPACE" "${IGNORE_NAMESPACES[@]}"; then
+        echo "Skipping namespace $NAMESPACE"
+        continue
     fi
 
-    # Get pods that are not in 'Running' or 'Succeeded' state
-    pods=$(kubectl get pods -n "$namespace" --field-selector=status.phase!=Running,status.phase!=Succeeded -o jsonpath='{.items[*].metadata.name}')
+    # Get all pods in the namespace that are not in 'Running' or 'Init' state
+    PODS=$(kubectl get pods -n $NAMESPACE -o jsonpath='{.items[?(@.status.phase!="Running" && @.status.phase!="Succeeded")].metadata.name}')
 
-    for pod in $pods; do
-      echo "Restarting pod $pod in namespace $namespace"
-      restart_pod "$namespace" "$pod"
+    for POD in $PODS; do
+        echo "Restarting pod $POD in namespace $NAMESPACE"
+        kubectl delete pod "$POD" -n "$NAMESPACE"
     done
-  done
-
-  sleep 600
 done
