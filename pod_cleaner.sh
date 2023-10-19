561b42e855f70b4d3911761c03f65a7b46e4bfcd
arkachat <arka13chatterjee@gmail.com>
2023-10-19 15:09:50 -0400

    added DOckerfile

--- pod_cleaner.sh
+++ pod_cleaner.sh
@@ -19,40 +19,28 @@ cleanup() {
     exit
 }
 
-# Register the cleanup function for script exit, SIGHUP, SIGINT, SIGQUIT, SIGABRT, SIGTERM
+# Register the cleanup function for script exit
 trap cleanup EXIT SIGHUP SIGINT SIGQUIT SIGABRT SIGTERM
 
-# Your existing script logic starts here
-
-# List of namespaces to ignore
-IGNORE_NAMESPACES=(kube-system)
-
-# Function to check if an item exists in an array
-in_array() {
-    local needle=$1
-    shift
-    local item
-    for item in "$@"; do
-        [[ $item == $needle ]] && return 0 # 0 is the success exit status in bash
-    done
-    return 1 # 1 is the general failure exit status in bash
+# Function to delete all pods in a namespace
+delete_pods_in_namespace() {
+    local namespace=$1
+    echo "Deleting all pods in namespace $namespace"
+    kubectl delete pods --all -n "$namespace"
 }
 
-# Get all namespaces
-NAMESPACES=$(kubectl get ns -o jsonpath='{.items[*].metadata.name}')
-
-for NAMESPACE in $NAMESPACES; do
-    # Skip the ignored namespace with IGNORED_NAMESPACES variable
-    if in_array "$NAMESPACE" "${IGNORE_NAMESPACES[@]}"; then
-        echo "Skipping namespace $NAMESPACE"
-        continue
-    fi
-
-    # Get all pods in the namespace that are not in 'Running' or 'Init' state
-    PODS=$(kubectl get pods -n $NAMESPACE -o jsonpath='{.items[?(@.status.phase!="Running" && @.status.phase!="Succeeded")].metadata.name}')
-
-    for POD in $PODS; do
-        echo "Restarting pod $POD in namespace $NAMESPACE"
-        kubectl delete pod "$POD" -n "$NAMESPACE"
+# Retrieve all namespaces, excluding those that contain "kube-"
+kubectl get namespaces -o name | grep -v "kube-" | awk -F "/" '{print $2}' | while read -r namespace ; do
+    # Delete all pods in the current namespace
+    delete_pods_in_namespace "$namespace" &
+    
+    # Allow only 10 jobs in parallel
+    while (( $(jobs | wc -l) >= 10 )); do
+        # Wait for any job to finish before starting a new one
+        wait -n
     done
 done
+
+# Wait for all background jobs to finish
+wait
+
